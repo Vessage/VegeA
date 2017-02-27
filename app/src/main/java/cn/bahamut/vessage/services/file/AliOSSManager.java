0542fdbca917136778365ba3d8785afe2ab85ba9
alexchow <cplover@live.com>
2017-02-27 19:02:43 +0800

    3.2.5 release

--- app/src/main/java/cn/bahamut/vessage/services/file/AliOSSManager.java
+++ app/src/main/java/cn/bahamut/vessage/services/file/AliOSSManager.java
@@ -131,7 +131,7 @@ public class AliOSSManager extends Observable{
         conf = new ClientConfiguration();
         conf.setConnectionTimeout(15 * 1000); // 连接超时，默认15秒
         conf.setSocketTimeout(15 * 1000); // socket超时，默认15秒
-        conf.setMaxConcurrentRequest(5); // 最大并发请求，默认5个
+        conf.setMaxConcurrentRequest(10); // 最大并发请求，默认5个
         conf.setMaxErrorRetry(2); // 失败后最大重试次数，默认2次
     }
 
